975424be384eb1a2be07d226b4ace683029f1a32
Lucifurry <thelucifurry@gmail.com>
2023-04-15 15:08:52 +0700

    Add `experimentalSet` files

--- src/sets/experimentalSet.ts
+++ src/sets/experimentalSet.ts
@@ -1,34 +1,46 @@
-import type { IFastSet, IFutureNativeSet } from '../types/interfaces'
-import {
-  difference,
-  intersection,
-  symmetricDifference,
-  union,
-} from '../core'
-
 import { ESign, MAX_BITS } from '../consts'
+import { difference, intersection, symmetricDifference, union } from '../core'
+import type { IFastSet, IFutureNativeSet } from '../types/interfaces'
 import { countBits } from '../utils/countBits'
 import { each } from '../utils/each'
 import { BaseSet } from './baseSet'
+import { UintSet } from './uintSet'
 
-export class UintSet extends BaseSet implements IFastSet, IFutureNativeSet<UintSet> {
-  static TAG = 'UintSet'
+export class ExperimentalSet extends BaseSet implements IFastSet, IFutureNativeSet<UintSet> {
+  static TAG = ExperimentalSet.name
+  _history: number[] = [] // TODO: Move to BaseSet
 
   constructor(items: number[] = []) {
     super()
+    this._history = []
     each(items, this.add.bind(this))
-  }
-
-  get size(): number {
-    let res = 0
-    each(this._[ESign.POSITIVE], bits => res += countBits(bits))
-    return res
   }
 
   add(uint: number): void {
     const byte = 1 << (uint % MAX_BITS)
     const index = Math.trunc(uint / MAX_BITS)
     this._[ESign.POSITIVE][index] |= byte
+    this._history.push(uint)
+  }
+
+  forEach(callback: (item: number) => void): void {
+    const items = [...new Set(this._history)]
+    const len = items.length
+    let i = -1
+    while (++i < len) {
+      const item = items[i]
+      if (this.has(item))
+        callback(item)
+    }
+    // TODO: Add removing unused items from list in next microtask
+  }
+
+  // Unchanged
+
+  get size(): number {
+    let res = 0
+    each(this._[ESign.POSITIVE], bits => res += countBits(bits))
+    return res
   }
 
   has(uint: number): boolean {
@@ -43,27 +55,6 @@ export class UintSet extends BaseSet implements IFastSet, IFutureNativeSet<UintS
     const data = this._[ESign.POSITIVE]
     if ((data[index] & byte) !== 0)
       data[index] ^= byte
-  }
-
-  forEach(callback: (item: number) => void): void {
-    const data = this._[ESign.POSITIVE]
-    const len = data.length
-    let cellIndex = -1
-    let item = 0
-    while (++cellIndex < len) {
-      if (!data[cellIndex]) {
-        item += MAX_BITS
-      }
-      else {
-        let i = -1
-        while (++i < MAX_BITS) {
-          if (data[cellIndex] & (1 << i))
-            callback(item)
-
-          item++
-        }
-      }
-    }
   }
 
   intersection(set: UintSet): UintSet {
