86b60aa73057f42f1cf267e870a5921840f443ec
Lucifurry <thelucifurry@gmail.com>
2023-04-15 08:43:34 +0700

    ...

--- src/sets/experimentalSet.ts
+++ src/sets/experimentalSet.ts
@@ -1,67 +1,92 @@
-import each from '../utils/each'
-import { MAX_ARRAY_LENGTH, MAX_BITS } from '../consts'
-import countBits from '../utils/countBits'
+import type { IFastSet, IFutureNativeSet } from '../types/interfaces'
+import {
+  difference,
+  intersection,
+  symmetricDifference,
+  union,
+} from '../core'
 
-export default class UintSet implements IFastSetUint {
-  static MAX_UINT = MAX_ARRAY_LENGTH * MAX_BITS
+import { ESign, MAX_BITS } from '../consts'
+import { countBits } from '../utils/countBits'
+import { each } from '../utils/each'
+import { BaseSet } from './baseSet'
 
+export class UintSet extends BaseSet implements IFastSet, IFutureNativeSet<UintSet> {
   static TAG = 'UintSet'
 
-  P: number[]
-
   constructor(items: number[] = []) {
-    this.P = []
+    super()
     each(items, this.add.bind(this))
   }
 
   get size(): number {
     let res = 0
-    each(this.P, bits => res += countBits(bits))
+    each(this._[ESign.POSITIVE], bits => res += countBits(bits))
     return res
   }
 
   add(uint: number): void {
     const byte = 1 << (uint % MAX_BITS)
     const index = Math.trunc(uint / MAX_BITS)
-    this.P[index] |= byte
+    this._[ESign.POSITIVE][index] |= byte
   }
 
   has(uint: number): boolean {
     const byte = 1 << (uint % MAX_BITS)
     const index = Math.trunc(uint / MAX_BITS)
-    return (this.P[index] & byte) !== 0
+    return (this._[ESign.POSITIVE][index] & byte) !== 0
   }
 
   delete(uint: number): void {
     const byte = 1 << (uint % MAX_BITS)
     const index = Math.trunc(uint / MAX_BITS)
-    this.P[index] &= 0xFFFF ^ byte
-  }
-
-  clear(): void {
-    this.P = []
+    const data = this._[ESign.POSITIVE]
+    if ((data[index] & byte) !== 0)
+      data[index] ^= byte
   }
 
   forEach(callback: (item: number) => void): void {
-    const data = this.P; const
-      len = data.length
-    for (let cellIndex = 0, item = 0; cellIndex < len; cellIndex++) {
+    const data = this._[ESign.POSITIVE]
+    const len = data.length
+    let cellIndex = -1
+    let item = 0
+    while (++cellIndex < len) {
       if (!data[cellIndex]) {
         item += MAX_BITS
-        continue
       }
-      for (let i = 0; i < MAX_BITS; i++, item++) {
-        if (data[cellIndex] & (1 << i))
-          callback(item)
+      else {
+        let i = -1
+        while (++i < MAX_BITS) {
+          if (data[cellIndex] & (1 << i))
+            callback(item)
+
+          item++
+        }
       }
     }
   }
 
-  values(): number[] {
-    const res: number[] = []
-    // const res: number[] = new Array(this.P.length);
-    let ix = 0
-    this.forEach(item => res[ix++] = item)
+  intersection(set: UintSet): UintSet {
+    const res = new UintSet()
+    intersection(res._[ESign.POSITIVE], this._[ESign.POSITIVE], set._[ESign.POSITIVE])
+    return res
+  }
+
+  union(set: UintSet): UintSet {
+    const res = new UintSet()
+    union(res._[ESign.POSITIVE], this._[ESign.POSITIVE], set._[ESign.POSITIVE])
+    return res
+  }
+
+  difference(set: UintSet): UintSet {
+    const res = new UintSet()
+    difference(res._[ESign.POSITIVE], this._[ESign.POSITIVE], set._[ESign.POSITIVE])
+    return res
+  }
+
+  symmetricDifference(set: UintSet): UintSet {
+    const res = new UintSet()
+    symmetricDifference(res._[ESign.POSITIVE], this._[ESign.POSITIVE], set._[ESign.POSITIVE])
     return res
   }
 }
