c814056200460c8c675e5d76808f4d046cd48bfa
Lucifurry <thelucifurry@gmail.com>
2023-04-15 09:25:20 +0700

    Ref: generate dts by `tsc` instead of `vite`-s plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [dts()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
