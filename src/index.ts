da81ab1e75d03b5aa55a4bb3d645eb20f829915a
Lucifurry <thelucifurry@gmail.com>
2023-04-15 15:10:10 +0700

    ...

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { Serializer } from './serializer'
 export { UintSet } from './sets/uintSet'
 export { IntSet } from './sets/intSet'
 // export { createUintSet } from './sets/functional/uintSet'; // Experimental functional logic
-// export * from './sets/experimentalSet';
+// export { ExperimentalSet } from './sets/experimentalSet'
