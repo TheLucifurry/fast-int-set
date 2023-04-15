c084ecaf5cf9720bba63b8a5277ceb133b98cc0b
Lucifurry <thelucifurry@gmail.com>
2023-04-15 15:08:14 +0700

    Ref: rename dist entrypoint

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'FastIntSet',
-      fileName: 'fast-int-set',
+      fileName: 'index',
       formats: ['es', 'umd'],
     },
     minify: 'terser'
