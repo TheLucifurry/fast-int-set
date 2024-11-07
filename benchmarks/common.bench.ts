b8e56f2618513b3b6791f0e681454baba890b92c
Lucifurry <thelucifurry@gmail.com>
2024-11-07 10:14:12 +0700

    Build: colorize benchmark titles

--- benchmarks/common.bench.ts
+++ benchmarks/common.bench.ts
@@ -3,6 +3,22 @@ import { bench, describe } from 'vitest'
 import { UintSet as FastIntSet } from '../dist/index.js'
 import { createArrayWithSteppedIntegers } from './utils.js'
 
+const colors = {
+  black: '\x1B[30m',
+  red: '\x1B[31m',
+  green: '\x1B[32m',
+  yellow: '\x1B[33m',
+  blue: '\x1B[34m',
+  magenta: '\x1B[35m',
+  cyan: '\x1B[36m',
+  white: '\x1B[37m',
+  gray: '\x1B[90m',
+}
+
+export function tint(color: keyof typeof colors, text: string) {
+  return `${colors[color]}${text}\x1B[0m`
+}
+
 const ITEM_RANGE = 100_000
 const SET_SIZES = [
   100,
@@ -15,17 +31,20 @@ const options: BenchOptions = {
   // iterations: 100, // Minimal iterations
 }
 
+const NATIVE_SET_TITLE = tint('gray', 'Native Set')
+const LIB_SET_TITLE = tint('green', 'FastIntSet')
+
 SET_SIZES
   .forEach((count) => {
     const items = createArrayWithSteppedIntegers(0, ITEM_RANGE, count)
     const suffix = ` (${count} items in the range up to ${ITEM_RANGE})`
 
     describe(`Create instance${suffix}`, () => {
-      bench('Native Set', () => {
+      bench(NATIVE_SET_TITLE, () => {
         const set = new Set(items)
       }, options)
 
-      bench('FastIntSet', () => {
+      bench(LIB_SET_TITLE, () => {
         const set = new FastIntSet(items)
       }, options)
 
@@ -35,7 +54,7 @@ SET_SIZES
     })
 
     describe(`Operations add/has/delete${suffix}`, () => {
-      bench('Native Set', () => {
+      bench(NATIVE_SET_TITLE, () => {
         const set = new Set(items)
         items.forEach((i) => {
           set.add(i)
@@ -44,7 +63,7 @@ SET_SIZES
         })
       }, options)
 
-      bench('FastIntSet', () => {
+      bench(LIB_SET_TITLE, () => {
         const set = new FastIntSet(items)
         items.forEach((i) => {
           set.add(i)
@@ -64,13 +83,13 @@ SET_SIZES
     })
 
     describe(`Operation forEach${suffix}`, () => {
-      bench('Native Set', () => {
+      bench(NATIVE_SET_TITLE, () => {
         const set = new Set(items)
         const res: number[] = []
         set.forEach(i => res.push(i))
       }, options)
 
-      bench('FastIntSet', () => {
+      bench(LIB_SET_TITLE, () => {
         const set = new FastIntSet(items)
         const res: number[] = []
         set.forEach(i => res.push(i))
