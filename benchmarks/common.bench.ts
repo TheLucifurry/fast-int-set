e1c8f60d4aea725070eafbc4654d2c73e810f966
Lucifurry <thelucifurry@gmail.com>
2024-11-07 10:49:17 +0700

    Build: Update benchmark code and result

--- benchmarks/common.bench.ts
+++ benchmarks/common.bench.ts
@@ -19,7 +19,7 @@ export function tint(color: keyof typeof colors, text: string) {
   return `${colors[color]}${text}\x1B[0m`
 }
 
-const ITEM_RANGE = 100_000
+const ITEM_RANGES = [10_000, 100_000]
 const SET_SIZES = [
   100,
   1_000,
@@ -29,76 +29,79 @@ const SET_SIZES = [
 const options: BenchOptions = {
   time: 1_000,
   // iterations: 100, // Minimal iterations
+  warmupIterations: 1_000,
 }
 
 const NATIVE_SET_TITLE = tint('gray', 'Native Set')
 const LIB_SET_TITLE = tint('green', 'FastIntSet')
 
-SET_SIZES
-  .forEach((count) => {
-    const items = createArrayWithSteppedIntegers(0, ITEM_RANGE, count)
-    const suffix = ` (${count} items in the range up to ${ITEM_RANGE})`
+ITEM_RANGES.forEach((ITEM_RANGE) => {
+  SET_SIZES
+    .forEach((count) => {
+      const items = createArrayWithSteppedIntegers(0, ITEM_RANGE, count)
+      const suffix = `([0..${ITEM_RANGE}] * ${count})`
 
-    describe(`Create instance${suffix}`, () => {
-      bench(NATIVE_SET_TITLE, () => {
-        const set = new Set(items)
-      }, options)
+      describe(`${tint('yellow', 'new')} ${suffix}`, () => {
+        bench(NATIVE_SET_TITLE, () => {
+          const set = new Set(items)
+        }, options)
 
-      bench(LIB_SET_TITLE, () => {
-        const set = new FastIntSet(items)
-      }, options)
+        bench(LIB_SET_TITLE, () => {
+          const set = new FastIntSet(items)
+        }, options)
 
-      // bench('Experimental FastIntSet', () => {
-      //   const set = new ExperimentalSet(items)
-      // }, options)
+        // bench('Experimental FastIntSet', () => {
+        //   const set = new ExperimentalSet(items)
+        // }, options)
+      })
+
+      describe(`${tint('blue', 'add/has/delete')} ${suffix}`, () => {
+        bench(NATIVE_SET_TITLE, () => {
+          const set = new Set(items)
+          items.forEach((i) => {
+            set.add(i)
+            set.has(i)
+            set.delete(i)
+          })
+        }, options)
+
+        bench(LIB_SET_TITLE, () => {
+          const set = new FastIntSet(items)
+          items.forEach((i) => {
+            set.add(i)
+            set.has(i)
+            set.delete(i)
+          })
+        }, options)
+
+        // bench('Experimental FastIntSet', () => {
+        //   const set = new ExperimentalSet(items)
+        //   items.forEach((i) => {
+        //     set.add(i)
+        //     set.has(i)
+        //     set.delete(i)
+        //   })
+        // }, options)
+      })
+
+      describe(`${tint('red', 'forEach')} ${suffix}`, () => {
+        bench(NATIVE_SET_TITLE, () => {
+          const set = new Set(items)
+          const res: number[] = []
+          set.forEach(i => res.push(i))
+        }, options)
+
+        bench(LIB_SET_TITLE, () => {
+          const set = new FastIntSet(items)
+          const res: number[] = []
+          set.forEach(i => res.push(i))
+        }, options)
+
+        // bench('Experimental FastIntSet', () => {
+        //   const set = new ExperimentalSet(items)
+        //   const res: number[] = []
+        //   set.forEach(i => res.push(i))
+        // }, options)
+      })
     })
-
-    describe(`Operations add/has/delete${suffix}`, () => {
-      bench(NATIVE_SET_TITLE, () => {
-        const set = new Set(items)
-        items.forEach((i) => {
-          set.add(i)
-          set.has(i)
-          set.delete(i)
-        })
-      }, options)
-
-      bench(LIB_SET_TITLE, () => {
-        const set = new FastIntSet(items)
-        items.forEach((i) => {
-          set.add(i)
-          set.has(i)
-          set.delete(i)
-        })
-      }, options)
-
-      // bench('Experimental FastIntSet', () => {
-      //   const set = new ExperimentalSet(items)
-      //   items.forEach((i) => {
-      //     set.add(i)
-      //     set.has(i)
-      //     set.delete(i)
-      //   })
-      // }, options)
-    })
-
-    describe(`Operation forEach${suffix}`, () => {
-      bench(NATIVE_SET_TITLE, () => {
-        const set = new Set(items)
-        const res: number[] = []
-        set.forEach(i => res.push(i))
-      }, options)
-
-      bench(LIB_SET_TITLE, () => {
-        const set = new FastIntSet(items)
-        const res: number[] = []
-        set.forEach(i => res.push(i))
-      }, options)
-
-      // bench('Experimental FastIntSet', () => {
-      //   const set = new ExperimentalSet(items)
-      //   const res: number[] = []
-      //   set.forEach(i => res.push(i))
-      // }, options)
-    })
-  })
+})
